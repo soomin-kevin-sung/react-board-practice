ed012773a105cb51043b47458c228e6d76a370fb
KevinSung <ssm0725@gmail.com>
2023-06-02 20:14:07 +0900

    글쓰기 기능 추가 완료

--- src/frontend/src/routes/BoardWrite.js
+++ src/frontend/src/routes/BoardWrite.js
@@ -20,6 +20,52 @@ const BoardWrite = () => {
       [name]: value,
     });
   }
+
+  const saveBoard = async () => {
+    await axios.post('/api/board', board).then((res) => {
+      alert('등록되었습니다.');
+      navigate('/board');
+    });
+  };
+
+  const backToList = () => {
+    navigate('/baord');
+  }
+
+  return (
+    <div>
+      <div>
+        <span>제목</span>
+        <input type="text" name="title" value={title} onChange={onChange} />
+      </div>
+      <br />
+      <div>
+        <span>작성자</span>
+        <input
+          type="text"
+          name="createdBy"
+          value={createdBy}
+          onChange={onChange}
+        />
+      </div>
+      <br />
+      <div>
+        <span>내용</span>
+        <textarea
+          name="contents"
+          cols="30"
+          rows="10"
+          value={contents}
+          onChange={onChange}
+        ></textarea>
+      </div>
+      <br />
+      <div>
+        <button onClick={saveBoard}>저장</button>
+        <button onClick={backToList}>취소</button>
+      </div>
+    </div>
+  );
 };
 
 export default BoardWrite;
